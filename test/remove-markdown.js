230c57376a516ba126fef78b9273278b0ee2b234
Jake Spurlock <whyisjake@gmail.com>
2017-06-20 20:32:14 -0700

    Update tests to use Chai, and expand the amount of tests.

--- test/remove-markdown.js
+++ test/remove-markdown.js
@@ -1,14 +1,54 @@
 'use strict';
+const expect = require('chai').expect;
+const removeMd = require('../');
 
-var fs = require('fs'),
-    path = require('path'),
-    should = require('should'),
-    removeMd = require('../'),
-    markdown = fs.readFileSync(path.resolve(__dirname, 'markdown.md')).toString(),
-    result = fs.readFileSync(path.resolve(__dirname, 'result.txt')).toString();
+describe('remove Markdown', function () {
+  describe('removeMd', function () {
+    it('should leave a string alone without markdown', function () {
+      const string = 'Javascript Developers are the best.';
+      expect(removeMd(string)).to.equal(string);
+    });
 
-describe('remove-markdown', function () {
-  it('should remove markdown', function () {
-    removeMd(markdown).should.eql(result);
+    it('should strip out remaining markdown', function () {
+      const string = '*Javascript* developers are the _best_.';
+      const expected = 'Javascript developers are the best.';
+      expect(removeMd(string)).to.equal(expected);
+    });
+
+    it('should leave non-matching markdown markdown', function () {
+      const string = '*Javascript* developers* are the _best_.';
+      const expected = 'Javascript developers* are the best.';
+      expect(removeMd(string)).to.equal(expected);
+    });
+
+    it('should leave non-matching markdown markdown, but strip empty anchors', function () {
+      const string = '*Javascript* [developers]()* are the _best_.';
+      const expected = 'Javascript developers* are the best.';
+      expect(removeMd(string)).to.equal(expected);
+    });
+
+    it('should strip anchors', function () {
+      const string = '*Javascript* [developers](https://engineering.condenast.io/)* are the _best_.';
+      const expected = 'Javascript developers* are the best.';
+      expect(removeMd(string)).to.equal(expected);
+    });
+
+    it('should strip img tags', function () {
+      const string = '![bear](https://placebear.com/640/480)*Javascript* developers are the _best_.';
+      const expected = 'Javascript developers are the best.';
+      expect(removeMd(string)).to.equal(expected);
+    });
+
+    it('should strip code tags', function () {
+      const string = 'In `Getting Started` we set up `something` foo.';
+      const expected = 'In Getting Started we set up something foo.';
+      expect(removeMd(string)).to.equal(expected);
+    });
+
+    it('should handle paragrahs with markdown', function () {
+      const paragraph = '##This is a heading##\n\nThis is a paragraph with [a link](http://www.disney.com/).\n\n### This is another heading\n\nIn `Getting Started` we set up `something` foo.\n\n  * Some list\n  * With items\n    * Even indented';
+      const expected = 'This is a heading\n\nThis is a paragraph with a link.\n\nThis is another heading\n\nIn Getting Started we set up something foo.\n\n  Some list\n  With items\n    Even indented';
+      expect(removeMd(paragraph)).to.equal(expected);
+    });
   });
 });
